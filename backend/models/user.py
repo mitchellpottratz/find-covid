b7aee57471a8734668d7a125508ed8fb033d077e
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-03-25 17:50:57 -0500

    created User model method to generate confirmation code

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,15 +1,24 @@
 from .base import BaseModel
-from peewee import CharField
+from peewee import CharField, BooleanField
 
 
 class User(BaseModel):
     first_name = CharField(max_length=100)
     last_name = CharField(max_length=100)
     phone_number = CharField(max_length=15)
+    phone_number_confirmed = BooleanField(default=False)
     sms_confirmation_code = CharField(max_length=10)
     password = CharField(max_length=255)
 
+    @staticmethod
+    def generate_sms_confirmation_code():
+        confirmation_code_list = []
 
+        for i in range(1, 15):
+            confirmation_code_list.append(str(random.randint(1, 9)))
+
+        return ''.join(confirmation_code_list)
+    
 
 
 
--- backend/resources/users.py
+++ backend/resources/users.py
@@ -29,7 +29,20 @@ def register():
         phone_number = data['phone_number']
         password = data['password']
 
-        
+        try:
+            user = User.get(User.phone_number == phone_number)
+
+            return jsonify(
+                data={},
+                status={
+                    'code': 409,
+                    'message': 'Phone number already exists'
+                }
+            )
+
+        # thrown if the provided phone number does not already exist
+        except DoesNotExist:
+            sms_confirmation_code = User.generate_sms_confirmation_code()
 
 
     # thrown if the request body is not valid
@@ -41,4 +54,6 @@ def register():
                 'message': 'Invalid request body'
             }
         )
+
+    
     
