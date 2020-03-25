8aa079d90b9746cb59aafa12b54ab42d44d691ac
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-03-25 17:11:10 -0500

    loaded the enviroment variables

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+# loads the enviroment variables
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env') 
+load_dotenv(dotenv_path)
+
 
 class Server:
 
     def __init__(self, app, login_manager):
         self.app = app
         self.login_manager = login_manager
-        self.DEBUG = True
-        self.PORT = 8000
+        self.DEBUG = os.environ['DEBUG']
+        self.PORT = os.environ['PORT']
 
     def start(self):
         print('Debug:', self.DEBUG)
