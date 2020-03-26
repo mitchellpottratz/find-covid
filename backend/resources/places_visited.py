5509f4613a6d2b006a145e061e9be0be08f2a417
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-03-26 11:17:33 -0500

    user can record a new place they have visited while sick

--- backend/resources/places_visited.py
+++ backend/resources/places_visited.py
@@ -25,4 +25,60 @@ def ping():
 
 
 
+# Create Route
+# this route is where a user can add places they have visited in the last 
+# 7 days of showing symptoms
+@places_visited.route('/', methods=['POST'])
+@login_required
+def create_place_visited():
+    try:
+        data = request.get_json()
+
+        date_visited = data['date_visited']
+        name = data['name']
+        address = data['address']
+
+        try:    
+            users_case = Case.get(Case.user == current_user.id)
+
+            new_place_visited = PlaceVisited.create(
+                case=users_case.user.id,
+                date_visited=date_visited,
+                name=name,
+                address=address
+            )
+
+            new_place_visited_dict = model_to_dict(new_place_visited)
+
+            return jsonify(
+                data=new_place_visited_dict,
+                status={
+                    'code': 201,
+                    'message': 'Successfully created a new visited place.'
+                }
+            )
+
+        # raised if the user has not created a case yet
+        except DoesNotExist:
+            return jsonify(
+                data={},
+                status={
+                    'code': 404,
+                    'message': 'Resource does not exist'
+                }
+            )
+    
+    except KeyError:
+        return jsonify(
+            data={},
+            status={
+                'code': 422,
+                'message': 'Invalid request body'
+            }
+        ) 
+
+
+
+
+
 
