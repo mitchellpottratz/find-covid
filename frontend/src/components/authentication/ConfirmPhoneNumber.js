2a211a0b8ae1b2ae611b83f8875e9b26ffa1f663
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-03-26 22:12:46 -0500

    added confirmation code input to the form

--- frontend/src/components/authentication/ConfirmPhoneNumber.js
+++ frontend/src/components/authentication/ConfirmPhoneNumber.js
@@ -6,6 +6,7 @@ import { loginUser } from '../../actions/userActions.js'
 
 // components
 import { Container, Row, Col, Card, Form } from 'react-bootstrap';
+import ReactCodeInput from 'react-verification-code-input';
 import FormButton from '../common/FormButton.js';
 import { Link, Redirect } from 'react-router-dom';
 
@@ -16,26 +17,56 @@ class ConfirmPhoneNumber extends React.Component {
     super(props);
 
     this.state = {
-			code: ''
+			confirmation_code: [],
+			isLoading: false
     }
 	}
 
-	handleChange = (e) => {
+	handleChange = (value) => {
 		this.setState({
-			code: e.target.value
+			confirmation_code: [...this.state.confirmation_code, value]
 		});
 	}
 	
 	render() {
 		return (
 			<Container>
-				<Row>
+				<Row className="py-4">
 					<Col></Col>
 
 					<Col md={ 6 } sm={ 12 }>
-						<Card>
+						<Card className="text-center px-4">
 							<Card.Body>
 								<Card.Title>Confirm your phone number</Card.Title>
+								<p>
+									Thank you for signing up { this.props.userInfo.first_name }!
+								</p>
+								<p>
+									Please enter the confirmation code we sent to: <br></br> 
+									<strong>{ this.props.userInfo.phone_number }</strong>
+								</p>	
+
+								<Form 
+									className="py-3"
+									onSubmit={ this.handleSubmit }>
+
+									<Form.Group>
+										<Form.Label>Enter Confirmation Code</Form.Label>
+										<ReactCodeInput 
+											className="d-inline-block"
+											type="number"
+											fields={ 5 } 
+											name='confirmation_code'
+											values={ this.state.confirmation_code }
+											onChange={ (value) => this.handleChange(value) } />
+									</Form.Group>	
+
+									<FormButton
+										className="d-inline-block"
+										variant="dark"
+										text="Confirm"
+										isLoading={ this.state.isLoading } />
+								</Form>
 
 							</Card.Body>
 						</Card>
