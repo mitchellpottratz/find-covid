ffb4e2558b3a48048e004ef68b843ac3dccba0cc
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-03-28 12:17:45 -0500

    modal to report a place visited can open and close

--- frontend/src/components/user/MyPlacesVisited.js
+++ frontend/src/components/user/MyPlacesVisited.js
@@ -18,18 +18,33 @@ class MyPlacesVisited extends React.Component {
 		}
 	}
 
+	showReportPlaceVisitedModal = () => {
+		this.setState({ showReportPlaceVisitedModal: true });
+	} 
+
+	hideReportPlaceVisitedModal = () => {
+		this.setState({ showReportPlaceVisitedModal: false });
+	} 
+
   render() {
   	return (
 			<React.Fragment>
-				<ReportPlaceVisitedModal />
+
+				{/* Modal for reporting a place the user has visited */}
+				<ReportPlaceVisitedModal 
+					showModal={ this.state.showReportPlaceVisitedModal }
+					hideModal={ this.hideReportPlaceVisitedModal }
+					/>
+
   			<div className="mt-4">  
 					<h5>Places Visited</h5>
 					<p>
-						Report all of the places you have visited within the last 7 of when you started 
-						to show symptoms.
+						Please report all of the places you have visited within the last 7 of when you started 
+						to show symptoms
 					</p>
 					<Button 
-						variant="light" >
+						variant="light" 
+						onClick={ this.showReportPlaceVisitedModal }>
 						Report Place Visited
 					</Button>
     		</div>
