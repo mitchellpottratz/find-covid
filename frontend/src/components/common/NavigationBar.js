94579223d7749fefc7c117c6f3a3824098b9bdb2
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-03-27 22:57:32 -0500

    created an account dropdown link on navbar

--- frontend/src/components/common/NavigationBar.js
+++ frontend/src/components/common/NavigationBar.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import { logoutUser } from '../../actions/userActions.js'
 
 // components
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 
 class NavigationBar extends React.Component {
@@ -16,23 +16,38 @@ class NavigationBar extends React.Component {
 
   render() {
     return (
-      <Navbar bg="dark" variant="dark">
+      <Navbar
+        id="navbar"
+        bg="dark" 
+        variant="dark">
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="/map">Map</Nav.Link>
-
-          {
-            this.props.isLoggedIn ? (
-              <Nav.Link onClick={ this.handleLogoutClick }>Logout</Nav.Link>
-            ) : (
-              <React.Fragment>
-                <Nav.Link href="/login">Login</Nav.Link>
-                <Nav.Link href="/register">Register</Nav.Link>
-              </React.Fragment>
-            )
-          }
-          
         </Nav>
+        <Nav>
+        {
+          this.props.isLoggedIn ? (
+            <React.Fragment>
+              <NavDropdown 
+                title="Account"
+                id="basic-nav-dropdown"
+                drop="">
+                <NavDropdown.Item href="#action/3.2">
+                  My Case
+                </NavDropdown.Item>
+                <NavDropdown.Item onClick={ this.handleLogoutClick }>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <Nav.Link href="/login">Login</Nav.Link>
+              <Nav.Link href="/register">Register</Nav.Link>
+            </React.Fragment>
+          )}
+        </Nav>
+
       </Navbar>
     )
   }
