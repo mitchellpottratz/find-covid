81af180394580649fd83a334293cc34fbe085a08
mitchellpottratz <mitchell.pottratz@yahoo.com>
2020-04-06 11:39:10 -0500

    added the logo to the homepage

--- frontend/src/components/home/HomeContainer.js
+++ frontend/src/components/home/HomeContainer.js
@@ -1,7 +1,9 @@
 import React from "react";
+import Logo from "../../logo.png";
 
-import { Container, Jumbotron } from "react-bootstrap";
+import { Container, Jumbotron, Image, Row, Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+
 
 class HomeContainer extends React.Component {
   constructor(props) {
@@ -13,16 +15,25 @@ class HomeContainer extends React.Component {
       <Container className="mt-4">
         <Jumbotron fluid>
           <Container className="text-center">
-            <h2 className="mb-4">
+            <Image src={Logo} width="100px" height="100px"/>
+            <h3 className="mt-3 mb-4">
               <strong>Know your risk of exposure</strong>
-            </h2>
-            <h3 className="mb-4">to COVID-19 the next time you</h3>
-            <h3>leave your house...</h3>
-            <Link className="btn btn-primary mt-4" to="/register">
-              Get Started
-            </Link>
+            </h3>
+            <h4 className="mb-4">to COVID-19 the next time you</h4>
+            <h4>leave your house...</h4>
+            <div class="d-block justify-content-center mt-4">
+              <Link className="btn btn-sm btn-primary mr-4" to="/map">
+                View Map
+              </Link>
+              <Link className="btn btn-sm btn-primary" to="/register">
+                Get Started
+              </Link>
+            </div>
           </Container>
         </Jumbotron>
+
+
+
       </Container>
     );
   }
