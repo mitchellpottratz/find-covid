5e889a23d616e0d9c35c8accef2263d20ca99287
mitchellpottratz <mitchell.pottratz@yahoo.com>
2020-04-06 10:42:01 -0500

    added help text to the MapSearchPopover component

--- frontend/src/components/help/MapSearchPopover.js
+++ frontend/src/components/help/MapSearchPopover.js
@@ -9,17 +9,38 @@ function MapSearchPopover(props) {
 
 	const popover = (
 		<Popover id="popover-basic">
-    	<Popover.Title as="h3">Popover right</Popover.Title>
+    	<Popover.Title as="h3">Searching for a place</Popover.Title>
     	<Popover.Content>
-      	And here's some <strong>amazing</strong> content. It's very engaging.
-      	right?
+      	<p>
+					<strong>
+						Do you want to know if someone with COVID-19 has visited your local
+						grocery store before you go?
+					</strong>
+				</p>
+				<p>
+					Start searching for a place nearby and you will be populated with the best matching 
+					businesses according to your search and current location. 
+				</p>
+				<p>
+					Once you find the place you are searching for, click on it, and if there is a red marker, that means
+					someone who has been tested positive or someone who has shown symptoms of COVID-19 has visited that business 
+					in the last 3 days.
+				</p>
+				<p>
+					You may then click on a marker to view more information about each case of COVID-19.
+				</p>
     	</Popover.Content>
   	</Popover>
 	)
 
   return (
-		<OverlayTrigger trigger="click" placement="right" overlay={ popover }>
-    	<FontAwesomeIcon className="text-primary" icon={ faInfoCircle } />
+		<OverlayTrigger 
+			trigger="click"
+			placement="right"
+			overlay={ popover }>
+			<FontAwesomeIcon 
+				className="info-icon ml-2"
+				icon={ faInfoCircle } />
   	</OverlayTrigger>
 	) 
 }
