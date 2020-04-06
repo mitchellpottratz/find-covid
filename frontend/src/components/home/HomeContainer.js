c5ce95c4ff9aed552dcc92efc6bf89201c35eb65
mitchellpottratz <mitchell.pottratz@yahoo.com>
2020-04-06 12:18:31 -0500

    added about cards to the homepage

--- frontend/src/components/home/HomeContainer.js
+++ frontend/src/components/home/HomeContainer.js
@@ -12,7 +12,7 @@ class HomeContainer extends React.Component {
 
   render() {
     return (
-      <Container className="mt-4">
+      <Container className="my-4">
         <Jumbotron fluid>
           <Container className="text-center">
             <Image src={Logo} width="100px" height="100px"/>
@@ -32,7 +32,63 @@ class HomeContainer extends React.Component {
           </Container>
         </Jumbotron>
 
+        <Row>
+          <Col></Col>
+          <Col md={6} sm={12}>
+            <Card>
+              <Card.Body>
+                <Card.Title className="text-center">What is Sympto+Map?</Card.Title>
+                <p>
+                  Sympto+Map provides users with an extra layer of safety and comfort during this 
+                  chaotic COVID-19 pandemic.
+                </p>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col></Col>
+        </Row>
 
+        <Row className="py-4">
+          <Col lg={2} md={1} sm={0}></Col>
+          <Col lg={4} md={5} sm={12} className="mt-3">
+            <Card className="h-100">
+              <Card.Body>
+                <Card.Title className="text-center">Report Your Case</Card.Title>
+                <p class="text-center">
+                  <strong>
+                    Have you been tested positive or shown symptoms of COVID-19?
+                  </strong>
+                </p>
+                <p>
+                  If so, it is crucial for your to <Link to="/register">sign up</Link> for Sympto+Map 
+                  right now. Once signed up, you can report all of the public places you have visited 
+                  while showing symptoms or being positive for COVID-19. The places you visited are 
+                  shown on the map for other users to see and take into precautions before going out 
+                  in public next.
+                </p>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={4} md={5} sm={12} className="mt-3">
+            <Card className="h-100">
+              <Card.Body>
+                <Card.Title className="text-center">Take Precaution</Card.Title>
+                <p className="text-center">
+                  <strong>
+                    Want to know if someone who goes to the same grocery has COVID-19?
+                  </strong>
+                </p>
+                <p>
+                  Sympto+Map's place search feature provides all users, whether signed up or not, with a solution. 
+                  Our place search feature allows users to search for places near them such as; grocery stores, 
+                  gas stations, restaurants, etc. To learn useful information about these places such as how many
+                  people have visited that place while being positive for COVID-19.
+                </p>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={2} md={1} sm={0}></Col>
+        </Row>
 
       </Container>
     );
