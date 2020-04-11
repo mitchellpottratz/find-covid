22204f9dbae7af3dfeafbba87597d3d5ac3bf6ee
mitchellpottratz <mitchell.pottratz@yahoo.com>
2020-04-11 09:06:13 -0500

    added the search results box below input

--- frontend/src/components/common/LiveSearchInput.js
+++ frontend/src/components/common/LiveSearchInput.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import { Form } from 'react-bootstrap';
+import { Form, ListGroup } from "react-bootstrap";
 
 class LiveSearchInput extends React.Component {
-
   constructor(props) {
     super(props);
   }
 
   render() {
     return (
-      <Form.Control 
-        type="text"
-         />
-    )
+      <div id="live-search-container">
+        <Form.Label> {this.props.label} </Form.Label>
+        <Form.Control type="text" placeholder={this.props.placeholder} />
+        <ListGroup 
+          id="live-search-box"
+          variant="flush">
+          <ListGroup.Item action> Cras justo odio </ListGroup.Item>
+          <ListGroup.Item action> Dapibus ac facilisis in </ListGroup.Item>
+          <ListGroup.Item> Morbi leo risus </ListGroup.Item>
+          <ListGroup.Item> Porta ac consectetur ac </ListGroup.Item>
+        </ListGroup>
+      </div>
+    );
   }
 }
 
 export default LiveSearchInput;
-
-
-
