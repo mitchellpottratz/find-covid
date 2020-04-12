95ee72e571e3a5b1962da63e58eafdf341625722
mitchellpottratz <mitchell.pottratz@yahoo.com>
2020-04-12 09:55:03 -0500

    created the form + styled for changing phone numbers

--- frontend/src/components/authentication/ChangePhoneNumber.js
+++ frontend/src/components/authentication/ChangePhoneNumber.js
@@ -5,19 +5,68 @@ import { connect } from "react-redux";
 import { confirmPhoneNumber } from "../../actions/userActions.js";
 
 // components
-import { Container, Row, Col, Card } from 'react-bootstrap'; 
+import { Container, Row, Col, Card, Form } from 'react-bootstrap'; 
+import PhoneInput from 'react-phone-number-input';
+import { Link } from 'react-router-dom';
+import FormButton from "../common/FormButton.js";
 
 
 class ChangePhoneNumber extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      phone_number: '',
+      isLoading: false
+    }
+  }
+
+  handlePhoneNumberChange = (value) => {
+    this.setState({ phone_number: value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('phone number:', this.state.phone_number);
   }
 
   render() {
     return (
       <Container>
-        <p>change phone number</p>
+        <Row className="py-4">
+          <Col></Col>
+          <Col md={6} sm={12}>
+            <Card className="text-center">
+              <Card.Body>
+                <div className="d-flex justify-content-between">
+                  <Link to="/confirm-number">
+                    Back
+                  </Link>
+                  <Card.Title>Change Phone Number</Card.Title>
+                  <span></span>
+                </div>
+                <Form 
+                  className="py-3"
+                  onSubmit={ this.handleSubmit }>
+                  <Form.Group className="pb-2">
+                    <Form.Label>Enter new phone number</Form.Label>
+                    <PhoneInput 
+                      placeholder="Enter your phone number"
+                      value={ this.state.phone_number }
+                      defaultCountry="US"
+                      onChange={ (value) => this.handlePhoneNumberChange(value) } />
+                  </Form.Group>
+                  <FormButton
+                    variant="primary"
+                    text="Change Number"
+                    isLoading={this.state.isLoading} />
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col></Col>
+        </Row>
       </Container>
     )
   }
